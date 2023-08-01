ab7365d2f075e8d9bc05e943c25c217082a5a926
Ossama <ossama.elhe@gmail..com>
2023-08-01 13:12:16 -0400

    vue

---
+++ precache-manifest.21ecba53c8522c7e0f4b7e32ae9c1396.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e763db0dcf4e73d9091",
+    "url": "/NB/css/app.82caa776.css"
+  },
+  {
+    "revision": "7e763db0dcf4e73d9091",
+    "url": "/NB/js/app.7ef44809.js"
+  },
+  {
+    "revision": "d96ab17704ade30a4034",
+    "url": "/NB/js/chunk-vendors.3e27bd71.js"
+  },
+  {
+    "revision": "c1733565b32b585676302d4233c39da8",
+    "url": "/NB/fonts/nucleo-icons.c1733565.eot"
+  },
+  {
+    "revision": "426439788ec5ba772cdf94057f6f4659",
+    "url": "/NB/fonts/nucleo-icons.42643978.woff2"
+  },
+  {
+    "revision": "0b8a30b10cbe7708d5f3a4b007c1d665",
+    "url": "/NB/img/nucleo-icons.0b8a30b1.svg"
+  },
+  {
+    "revision": "2569aaea6eaaf8cd210db7f2fa016743",
+    "url": "/NB/fonts/nucleo-icons.2569aaea.woff"
+  },
+  {
+    "revision": "f82ec6ba2dc4181db2af35c499462840",
+    "url": "/NB/fonts/nucleo-icons.f82ec6ba.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/NB/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/NB/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/NB/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/NB/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/NB/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "5c9865523230321c607bb787e7b87e79",
+    "url": "/NB/index.html"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/NB/favicon.png"
+  },
+  {
+    "revision": "9a30966b0a979b3c3854d65fa45180d0",
+    "url": "/NB/img/brand/blue.png"
+  },
+  {
+    "revision": "8e55eab46b5fcfc4a7a0b27cb07c8888",
+    "url": "/NB/img/brand/github-white-slim.png"
+  },
+  {
+    "revision": "c85c75275c0a0a617f9e5accc2700908",
+    "url": "/NB/img/brand/creativetim-white-slim.png"
+  },
+  {
+    "revision": "b9949387c6179e2dc4c675134a7b7935",
+    "url": "/NB/img/brand/favicon.png"
+  },
+  {
+    "revision": "0f7512371e5c0efb61222fba8fb40ff7",
+    "url": "/NB/img/brand/white.png"
+  },
+  {
+    "revision": "594b1ee1d95ada356eaad078e9217932",
+    "url": "/NB/img/ill/ill-2.svg"
+  },
+  {
+    "revision": "fd4a34d026fb9e0f4867188d47b11ba8",
+    "url": "/NB/img/theme/img-1-1200x1000.jpg"
+  },
+  {
+    "revision": "7789b5bfa57722dd8916b1b9ff1b1d37",
+    "url": "/NB/img/theme/img-2-1200x1000.jpg"
+  },
+  {
+    "revision": "20d702b83a06bdb2ea71c4c0cb9a7a56",
+    "url": "/NB/img/theme/profile.jpg"
+  },
+  {
+    "revision": "dc49ad52655e1d9d0552c026db3ef688",
+    "url": "/NB/img/theme/landing.jpg"
+  },
+  {
+    "revision": "edc7106b21ec12e57022b2ebd534cd2d",
+    "url": "/NB/img/theme/team-1-800x800.jpg"
+  },
+  {
+    "revision": "974088a1931e40895bac6db119c62448",
+    "url": "/NB/img/theme/promo-1.png"
+  },
+  {
+    "revision": "54e3f3c414bd8e7234bae3ee3be950e5",
+    "url": "/NB/img/theme/team-3-800x800.jpg"
+  },
+  {
+    "revision": "da10038f094b1837dfba043f255a9162",
+    "url": "/NB/img/theme/team-4-800x800.png"
+  },
+  {
+    "revision": "be997d5226b992ffad34816870c6b7aa",
+    "url": "/NB/img/theme/team-2-800x800.jpg"
+  }
+];
